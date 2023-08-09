f1427a041ba8e75d7c813080f0a1260e79119256
rajaji49 <73927131+rajaji49@users.noreply.github.com>
2023-08-09 21:58:24 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3JSZOJBQ3JDNG43U"
-  secret_key = "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"
+  access_key = "AKIA36HXPMW67KYB2H4A"
+  secret_key = "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"
 }
 
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-chinthasdfghjcvbnm-bucket"
+  bucket = "s3-b1-vcube-practiceq"
 
   acl    = "private"   # or can be "public-read"
 
